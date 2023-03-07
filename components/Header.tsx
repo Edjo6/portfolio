4c20f274d8af5884ec07a5420ae6444f269d4d46
Edwin J <ej223ve@student.lnu.se>
2023-03-07 11:43:48 +0100

    Changed to link

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,38 +1,26 @@
-import { useRouter } from "next/router";
+import Link from "next/link";
 
 export default function Header() {
-
-  const router = useRouter();
-
-  const handleHomeClick = () => {
-    router.push("/");
-  };
-
-  const handleAboutClick = () => {
-    router.push("/about");
-  };
-
-
   return (
     <header>
       <nav className="flex flex-row justify-center bg-dark max-h-fit p-5">
 
         <div className="p-5 gap-5">
-          <button className="bg-light text-orangina font-bold p-2 hover:bg-primary hover:scale-105 rounded-full shadow-2xl" onClick={handleHomeClick}>
+          <Link href="/" className="bg-light text-orangina font-bold p-2 hover:bg-primary hover:scale-105 rounded-full shadow-2xl">
             HOME
-          </button>
+          </Link>
         </div>
 
         <div className="p-5 gap-5">
-          <button className="bg-light text-orangina font-bold  p-2 hover:bg-primary hover:scale-105 rounded-full shadow-2xl">
+          <Link href="/resume" className="bg-light text-orangina font-bold  p-2 hover:bg-primary hover:scale-105 rounded-full shadow-2xl">
             RESUMÈ
-          </button>
+          </Link>
         </div>
 
         <div className="p-5 gap-5">
-          <button className="bg-light text-orangina  font-bold p-2 hover:bg-primary hover:scale-105 rounded-full shadow-2xl" onClick={handleAboutClick}>
+          <Link href="/about" className="bg-light text-orangina  font-bold p-2 hover:bg-primary hover:scale-105 rounded-full shadow-2xl">
             ABOUT ME
-          </button>
+          </Link>
         </div>
 
       </nav>
